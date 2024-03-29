065d518ad80fce45ebdb92a970f607ff058768db
Kritam123 <ktmdah01@gmail.com>
2024-03-29 16:17:45 +0545

    fix error

--- lib/getCurrentUser.ts
+++ lib/getCurrentUser.ts
@@ -3,11 +3,13 @@ import { db } from "./db";
 
 export const getCurrentUser = async () => {
   const user = await currentUser();
+  if(!user) {
+    return null;
+  }
   const dbUser = await db.user.findUnique({
     where: {
       externalUserId: user?.id,
     },
-
     include: {
       fullCodes: {
         orderBy: {
